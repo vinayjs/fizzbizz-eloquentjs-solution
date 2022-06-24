8b2173a1f45c751f172e6c5e73cfa56179df2553
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-06-24 10:47:28 +0530

    Add files via upload

---
+++ ejs.js
@@ -0,0 +1,17 @@
+
+let num = 0;
+for(let num = 0;num<=100;num++){
+    if(num%3==0){
+        console.log("fizz");
+    }else if(num%5==0 && num%3!=0){
+        console.log("bizz");
+    }else (console.log(num));
+}
+
+/*let num = 0;
+for(let num = 0;num<=100;num++){
+    if(num%5==0 && num%3==0){
+        console.log("fizzbizz");
+    }else console.log(num);
+}
+*/
